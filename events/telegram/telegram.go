02257fc8a931394226fb3aebbaf1d9a087877c35
Mikhail <skulllalka@gmail.com>
2025-12-17 22:47:05 +0300

    to PC

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,11 +1,14 @@
 package telegram
 
-import "github.com/Skulllalka/bot_on_go/clients/telegram"
+import (
+	"github.com/Skulllalka/bot_on_go/clients/telegram"
+	"github.com/Skulllalka/bot_on_go/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
 func New(client *telegram.Client)
