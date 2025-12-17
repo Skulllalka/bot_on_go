a750dfa5c637665909f9f9a764949accb06f3eb7
Mikhail <skulllalka@gmail.com>
2025-12-17 17:10:50 +0300

    crated decodePage func with some small helping funtions

--- storage/files/files.go
+++ storage/files/files.go
@@ -2,8 +2,11 @@ package files
 
 import (
 	"encoding/gob"
+	"errors"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"time"
 
 	e "github.com/Skulllalka/bot_on_go/lib"
 	"github.com/Skulllalka/bot_on_go/storage"
@@ -12,6 +15,8 @@ import (
 const (
 	defaultPerm = 0774
 )
+
+var errNoSavedPages = errors.New("page not saved")
 
 type Storage struct {
 	basePath string
@@ -50,6 +55,38 @@ func (s Storage) Save(page *storage.Page) (err error) {
 	}
 
 	return nil
+}
+
+func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
+	filePath := filepath.Join(s.basePath, userName)
+
+	files, err := os.ReadDir(filePath)
+	if err != nil {
+		return nil, e.Wrap("can't open dir", err)
+	}
+	if len(files) == 0 {
+		return nil, errNoSavedPages
+	}
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(files))
+	file := files[n]
+	return s.decodePage(filepath.Join(filePath, file.Name()))
+}
+
+func (s Storage) decodePage(filePath string) (*storage.Page, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, e.Wrap("cannot open file", err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	var p storage.Page
+	if err := gob.NewDecoder(f).Decode(&p); err != nil {
+		return nil, e.Wrap("cant decode file", err)
+	}
+	return &p, nil
 }
 
 func fileName(p *storage.Page) (string, error) {
