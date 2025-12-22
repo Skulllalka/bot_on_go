bc23dca9b721f4f2f207464c26b4f36a7c1ece0b
Mikhail <skulllalka@gmail.com>
2025-12-22 23:48:52 +0300

    feat: Add SQLite storage implementation

---
+++ storage/sqlite/sqlite.go
@@ -0,0 +1,69 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+
+	e "github.com/Skulllalka/bot_on_go/lib"
+	"github.com/Skulllalka/bot_on_go/storage"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(path string) (*Storage, error) {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, e.Wrap("can't connect to db", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, e.Wrap("can't ping db", err)
+	}
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) Save(ctx context.Context, p *storage.Page) error {
+	query := `INSERT INTO pages (url, username) VALUES (?, ?)`
+	if _, err := s.db.ExecContext(ctx, query, p.URL, p.UserName); err != nil {
+		return e.Wrap("can't write a query", err)
+	}
+	return nil
+}
+
+func (s *Storage) PickRandom(ctx context.Context, username string) (*storage.Page, error) {
+	query := `SELECT url FROM pages WHERE user_name=? ORDER BY RANDOM() LIMIT 1`
+	var url string
+	err := s.db.QueryRowContext(ctx, query, username).Scan(&url)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, e.Wrap("can't pick url", err)
+	}
+	return &storage.Page{
+		URL:      url,
+		UserName: username,
+	}, nil
+}
+
+func (s *Storage) Remove(ctx context.Context, p *storage.Page) error {
+	query := `DELETE FROM pages WHERE url = ? AND user_name = ?`
+
+	if _, err := s.db.ExecContext(ctx, query, p.URL, p.UserName); err != nil {
+		return e.Wrap("can't remove page", err)
+	}
+	return nil
+}
+
+func (s *Storage) IsExists(ctx context.Context, p *storage.Page) (bool, error) {
+	query := `SELECT COUNT(*) FROM pages WHERE url = ? AND user_name = ?`
+
+	var count int
+	err := s.db.QueryRowContext(ctx, query, p.URL, p.UserName).Scan(&count)
+	if err != nil {
+		return false, e.Wrap("can't get query", err)
+	}
+	return count > 0, nil
+}
